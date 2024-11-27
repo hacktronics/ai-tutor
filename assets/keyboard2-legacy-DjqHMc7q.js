97410173a7f14f2986b5830be1c93edb44bf0b61
Hacktronics India <support@hacktronics.co.in>
2024-11-27 22:12:35 +0530

    Update

--- assets/keyboard2-legacy-DjqHMc7q.js
+++ assets/keyboard2-legacy-DjqHMc7q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFodyQZw.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DpVzc7h2.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
